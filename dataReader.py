b4acedd185b3a5ab509533d4cebe6637b8b03145
shduke <seanhudson96@gmail.com>
2015-10-03 22:42:04 -0400

    code

--- dataReader.py
+++ dataReader.py
@@ -5,19 +5,38 @@ Created on Oct 3, 2015
 '''
 import requests 
 
-#output = open("potholeData.csv", "w")
+def potholeChecker(vibration, dif):
+    if vibration > 0 and dif > 0:
+        return True
+    return False
+        #threshhold for a derivative
 
-r = requests.get('http://riot-hackathon.bright-wolf.net/api/techsmart/sensors/history?sensor_id=1000038&from=2015-10-02T15:41:23:149Z', auth=('alim.ladha', 'wolfpuck'))
-#'http://riot-hackathon.bright-wolf.net/api/sensors?org_id=techsmart/'
+output = open("potholeData.csv", "w")
+
+r = requests.get('http://riot-hackathon.bright-wolf.net/api/sensors/history?sensor_id=1000038&from=2015-10-02T15:41:23Z', auth=('alim.ladha', 'wolfpuck'))
+
 print r.status_code
-#r.headers['application/json; charset=utf8']
-#'application/json; charset=utf8'
 r.encoding
-#'utf-8'
 r.text
-#u'{"type":"User"...'
-r.json()
+x = r.json()
+matches =  x['queryresult']['matches']
+elevation1 = None
+elevation2 = None
+for entry in matches:
+    point = ''
+    vibration = entry['analog_channel_1']
+    if elevation1 == None:
+        elevation1 = entry['analog_channel_3']
+        continue
+    elevation2 = elevation1
+    elevation1 = entry['analog_channel_3']
+    location = {'latitude' : 35.7694934, 'longitude' :-78.6790169, 'altitude' : 0.0}
+    latitude = location['latitude']
+    longitude = location['longitude']
+    #print (str(vibration) + ',' + str(elevation) + ',' + 'location')
+    dif = elevation2-elevation1
+    if potholeChecker(vibration, dif):
+        output.write((str(vibration) + ',' + str(dif) + ',' + str(latitude) + ',' + str(longitude) + '\n'))
+    output.flush()
 
-
-
-#output.close()
+output.close()
