76a0f20e9fa296c2c39f3e33332b76ca9554e7bf
shduke <seanhudson96@gmail.com>
2015-10-04 13:22:44 -0400

    accurate

--- dataReader.py
+++ dataReader.py
@@ -1,15 +1,16 @@
-'''
-Created on Oct 3, 2015
-
-@author: seanhudson
-'''
 import requests 
+from random import randint
 
 def potholeChecker(vibration, dif):
-    if vibration > 0 and dif > 0:
+    if vibration > 0 and (dif > 40 and dif < 150):
         return True
     return False
         #threshhold for a derivative
+
+def locationGenerator(change):         #generates random coordinates along I-95
+    x = 34.535730 + change
+    y = (-79.233051 + 1.014439 * (change))
+    return (x,y)
 
 output = open("potholeData.csv", "w")
 
@@ -22,6 +23,7 @@ x = r.json()
 matches =  x['queryresult']['matches']
 elevation1 = None
 elevation2 = None
+change = 0.0      #used for testing
 for entry in matches:
     point = ''
     vibration = entry['analog_channel_1']
@@ -30,11 +32,14 @@ for entry in matches:
         continue
     elevation2 = elevation1
     elevation1 = entry['analog_channel_3']
-    location = {'latitude' : 35.7694934, 'longitude' :-78.6790169, 'altitude' : 0.0}
+    #location = {'latitude' : 35.7694934, 'longitude' :-78.6790169, 'altitude' : 0.0}
+    local = locationGenerator(change)
+    location = {'latitude' : local[0], 'longitude' :local[1], 'altitude' : 0.0}
     latitude = location['latitude']
     longitude = location['longitude']
     #print (str(vibration) + ',' + str(elevation) + ',' + 'location')
     dif = elevation2-elevation1
+    change = change + .001111
     if potholeChecker(vibration, dif):
         output.write((str(vibration) + ',' + str(dif) + ',' + str(latitude) + ',' + str(longitude) + '\n'))
     output.flush()
